2681fe025a7c049ae37516fc3d6b4a14963a6eb7
Am_I_a_person <lhy157571077752020@163.com>
2023-05-14 18:20:51 +0800

    个人博客

--- js/md.js
+++ js/md.js
@@ -1,21 +1,62 @@
 'use scrict'
 
+import('../mysql-master');
 
-function get_html(id) {
-    let selector = '.'+id;
+const cla = {
+    "python": 1,
+    "C/C++": 2,
+    "markdown": 3,
+    "MySQL": 4,
+    "java": 5,
+    "ХЬАн": 6
+};
+
+function get_html() {
+    let selector = '.editormd-preview';
     return $(selector).get(0);
 }
 
-function set() {
-    let selector = '#layout';
-    let a = $(selector).html(get_html('markdown-body'));
-    $(selector).css('width', '710px');
+function get_title() {
+    let title = '#t1';
+    return $(title).val();
 }
 
+function get_class() {
+    let cl = 'input[name=\'cl\']:checked';
+    let checked_arr = [];
+    for (let i = 0; i < $(cl).length; i++) {
+        checked_arr.push($(cl)[i].value);
+    }
+    return checked_arr;
+}
+  
+function get_connect() {
+    let mysql = require("mysql");
+    let connection = mysql.createConnection({
+        user: 'root',
+        password: '237161',
+        host: 'localhost',
+        port: '3306',
+        database: 'myweb'
+    });
+    connection.connect();
+    return connection;
+}
 
-$('.blogs').click(function () {
-    console.log($(this).get(0));
-})
+function submit() {
+    let title = get_title();
+    let classification = get_class();
+    let context = get_html();
+    let conn = get_connect();
+    for (let i = 0; i < classification.length; i++) {
+        let sql = "insert into `blogs` (title, context, create_date, update_date, auther_id, classification_id) " +
+            "values (\'" + title + "\',\'" + context.innerHTML + "\',\'" + new Date().toLocaleString() + "\',\'" + new Date().toLocaleString() + "\',1," + cla[classification[i]] + ");"
+        console.log(sql);
+        conn.query(sql);
+    }
+    conn.end();
 
-console.log(get_html('test-editormd'));
-
+    console.log(title);
+    console.log(classification);
+    console.log(context);
+}
